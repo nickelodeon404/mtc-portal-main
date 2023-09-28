acfa3cc7c9416d1d79c111c01496c66c619c00e1
Rommel Ramos <122844843+eyshelvi@users.noreply.github.com>
2023-09-28 09:35:04 +0800

    bar chart

--- resources/views/registrar/index.blade.php
+++ resources/views/registrar/index.blade.php
@@ -16,7 +16,7 @@
                 <ol class="breadcrumb mt-4">
                     <li class="breadcrumb-item active">Dashboard</li>
                 </ol>
-                <h1 class="mt-4">Dashboard</h1>
+                <h1 class="mt-4"> Dashboard</h1>
                 <div class="row">
                     <div class="col-xl-3 col-md-6">
                         <div class="card bg-dark text-white mb-4">
@@ -43,8 +43,61 @@
                         </div>
                     </div>
                 </div>
+            
+                    
+                    <!-- Centered Column for Bar Graph -->
+                    <div class="col-md-6 offset-md-3">
+                        <div class="card">
+                            <div class="card-header">
+                                Bar Graph
+                            </div>
+                            <div class="card-body">
+                                <canvas id="barGraph"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
         <x-footer />
     </x-panel>
+
+    <!-- Include Chart.js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
+
+    <!-- JavaScript to create and populate the bar graph -->
+    <script>
+        var ctx = document.getElementById('barGraph').getContext('2d');
+        var data = {
+            labels: ['Label 1', 'Label 2', 'Label 3'], // Your labels here
+            datasets: [{
+                label: 'Data',
+                data: [10, 20, 30], // Your data values here
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                ],
+                borderWidth: 1
+            }]
+        };
+        var config = {
+            type: 'bar',
+            data: data,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        };
+        var myChart = new Chart(ctx, config);
+    </script>
+</script>
 @endsection
