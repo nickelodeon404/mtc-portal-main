07c0c08e602abe3b8e590d237b68b5bccfd65c2a
Rommel Ramos <122844843+eyshelvi@users.noreply.github.com>
2023-10-22 21:23:24 +0800

    Update view.blade.php

--- resources/views/registrar/view.blade.php
+++ resources/views/registrar/view.blade.php
@@ -115,7 +115,6 @@
                                                 </button>
                                             </form>
                                         </td>
-                                        d>
                                     </tr>
                                 @endforeach
                             </tbody>
