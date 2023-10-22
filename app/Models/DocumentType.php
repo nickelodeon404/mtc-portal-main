9efbcdfcea796c427a61bc9bea69263b023e5a03
Rommel Ramos <122844843+eyshelvi@users.noreply.github.com>
2023-10-22 19:13:59 +0800

    mtc-portal

--- app/Models/DocumentType.php
+++ app/Models/DocumentType.php
@@ -1,15 +1,31 @@
 <?php
 
-namespace App\Models;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class DocumentType extends Model
+return new class extends Migration
 {
-    use HasFactory;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('document_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('student')->nullable(false);
+            $table->string('mobile_number')->nullable(false);
+            $table->string('document_type')->nullable(false);
+            $table->string('purpose')->nullable(false);
+            $table->timestamps();
+        });
+    }
 
-    protected $table = 'document_types';
-    protected $primaryKey = 'id';
-    protected $fillable = ['student', 'document_type', 'purpose'];
-}
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('document_types');
+    }
+};
