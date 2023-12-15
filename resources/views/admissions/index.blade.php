67b486a4f0198781b2b764a65a59a3256e4c12d9
Jonathan Beloro <jonbeloro@my.cspc.edu.ph>
2023-12-15 19:29:37 +0800

    updates

--- resources/views/admissions/index.blade.php
+++ resources/views/admissions/index.blade.php
@@ -19,28 +19,6 @@
     </div>
 </header>
 
-<!-- Admission-->
-<section class="page-section" id="admission" style="background-color: #ede2c3de; padding: 20px;">
-    <div class="containe@extends('layouts.app')
-@section('content')
-<head>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-</head>
-<!-- Masthead -->
-<header class="masthead">
-    <div class="container px-4 px-lg-5 h-100">
-        <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
-            <div class="col-lg-8 align-self-end">
-                <h1 class="text-white font-weight-bold">Welcome, Future Mathesians!</h1>
-                <hr class="divider" />
-            </div>
-            <div class="col-lg-8 align-self-baseline">
-                <p class="text-white-75 mb-5"></p>
-                <a class="btn btn-primary btn-xl" href="#admission">Proceed to admission</a>
-            </div>
-        </div>
-    </div>
-</header>
 
 <!-- Admission-->
 <section class="page-section" id="admission" style="background-color: #ede2c3de; padding: 20px;">
