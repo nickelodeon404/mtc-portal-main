826d3a4a921bb2c326afb7bc8c413e9ded7da5a2
Rommel Ramos <122844843+eyshelvi@users.noreply.github.com>
2023-12-18 01:40:15 +0800

    Update index.blade.php

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -6,12 +6,19 @@
     <x-panel>
         <main>
             <style>
+                /* Global Styles */
+                body {
+                    background-color: #f0f0f0;
+                }
+
+                /* Welcome Message Styles */
                 .welcome-message-container {
                     display: flex;
                     flex-direction: column;
                     align-items: center;
                     justify-content: center;
-                    height: 100vh;
+                    height: 50vh; /* Adjusted height for better spacing */
+                    margin-bottom: 20px;
                 }
 
                 .welcome-message {
@@ -23,10 +30,8 @@
                     border-radius: 20px;
                     box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
                     text-align: center;
-                }
-
-                /* Add a fade-in animation */
-                .welcome-message {
+                    margin: 0;
+                    /* Add a fade-in animation */
                     animation: fadeIn 2s ease-in-out, bounce 2s infinite;
                 }
 
@@ -50,219 +55,103 @@
                         transform: translateY(-10px);
                     }
                 }
+
+                /* Calendar Styles */
+                .event-calendar {
+                    background-color: #5c2c784e;
+                    border-radius: 10px;
+                    padding: 20px;
+                    margin-bottom: 20px; /* Adjusted margin */
+                }
+
+                .event-calendar h2 {
+                    font-size: 24px;
+                    color: #333333;
+                    margin-bottom: 20px;
+                    text-align: center;
+                }
+
+                .fc-day {
+                    background-color: #ffffff;
+                    border: 1px solid #c3cee4;
+                    border-radius: 5px;
+                }
+
+                .fc-day:hover {
+                    background-color: #f0f0f0;
+                }
+
+                .fc-event {
+                    background-color: #3498db;
+                    border: none;
+                    border-radius: 5px;
+                    color: #ffffff;
+                }
+
+                .fc-event:hover {
+                    background-color: #2b7cba;
+                }
             </style>
+
             <div class="container-fluid px-4 mt-4">
-                            @if(session('success'))
-                                <div class="alert alert-success">
-                                    {{ session('success') }}
-                                </div>
-                            @endif
+                @if(session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
                 <div class="row justify-content-center">
                     <div class="col-md-8">
                         <div class="welcome-message-container">
                             <h1 class="mt-4 mb-4 welcome-message">Welcome, {{ Auth::user()->name }}!</h1>
                         </div>
                     </div>
-                </div>                
-            </div>
+                </div>
 
-            <!-- Event Calendar -->
-            <div class="row justify-content-center mt-4">
-                <div class="col-md-10">
-                    <div class="card">
-                        <div class="card-body event-calendar">
-                            <h2 class="text-center">Event Calendar</h2>
+                <!-- Event Calendar -->
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card event-calendar">
+                            <h2>Event Calendar</h2>
                             <div id="calendar"></div>
                         </div>
                     </div>
                 </div>
+                <!-- End Event Calendar -->
             </div>
-            <!-- End Event Calendar -->
         </main>
         <x-footer />
-    </x-panel>  
-</style>
+    </x-panel>
 
- <!-- FullCalendar CSS and JavaScript -->
+    <!-- FullCalendar CSS and JavaScript -->
     <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
 
-
-    <!-- Custom CSS for Event Calendar -->
-    <style>
-
-        /* Set background color for the entire page */
-        body {
-            background-color: #f0f0f0; /* Adjust the color as needed */
-        }
-
-        .event-calendar {
-            background-color: #5c2c784e ;
-            border-radius: 10px;
-            padding: 20px;
-        }
-
-        .event-calendar h2 {
-            font-size: 24px;
-            color: #333333;
-            margin-bottom: 20px;
-        }
-
-        .fc-day {
-            background-color: #ffffff;
-            border: 1px solid #c3cee4;
-            border-radius: 5px;
-        }
-
-        .fc-day:hover {
-            background-color: #f0f0f0;
-        }
-
-        .fc-event {
-            background-color: #3498db;
-            border: none;
-            border-radius: 5px;
-            color: #ffffff;
-        }
-
-        .fc-event:hover {
-            background-color: #2b7cba;
-        }
-    </style>
-
     <!-- JavaScript for Event Calendar -->
-    
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             var calendarEl = document.getElementById('calendar');
             var calendar = new FullCalendar.Calendar(calendarEl, {
                 initialView: 'dayGridMonth',
-                editable: true,
-                selectable: true,
-                selectHelper: true,
-                events: {
-                    url: '{{ route("get-event") }}',
-                    method: 'GET',
-                    extraParams: {
-                        _token: '{{ csrf_token() }}'
-                    },
-                    failure: function () {
-                        alert('There was an error while fetching events!');
+                editable: false, // Disable editing
+                selectable: false, // Disable selection
+                events: [
+                    {
+                        title: 'Christmas Day',
+                        start: '2023-12-25',
+                        allDay: true
                     }
-                },
-                eventBackgroundColor: '#3498db',
-                eventTextColor: '#ffffff',
+                ],
                 headerToolbar: {
                     left: 'prev,next today',
                     center: 'title',
                     right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
                 },
                 displayEventTime: false,
-                dateClick: function (info) {
-                    var title = prompt('Event Title:');
-                    var startDate = prompt('Start Date and Time (YYYY-MM-DD HH:mm):');
-                    var endDate = prompt('End Date and Time (YYYY-MM-DD HH:mm):');
+            });
 
-                    if (title) {
-                       
-                        calendar.addEvent({
-                            title: title,
-                            start: startDate,
-                            end: endDate,
-                            allDay: false // Assuming events can have specific times
-                        });
-
-                        // Send AJAX request to store the event in the database
-                        $.ajax({
-                            url: '{{ route("calendar-events.store") }}',
-                            type: 'POST',
-                            data: {
-                                title: title,
-                                start: startDate,
-                                end: endDate,
-                                _token: '{{ csrf_token() }}'
-                            },
-                            success: function (response) {
-                                calendar.refetchEvents();
-                                console.log(response);
-                                calendar.refetchEvents();
-                            },
-                            error: function (error) {
-                                console.error(error);
-                            }
-                        });
-                    }
-                },
-                eventClick: function (info) {
-                var action = prompt('Do you want to edit or delete this event? Type "edit" or "delete":');
-
-                if (action === 'edit') {
-                    editEvent(info);
-                } else if (action === 'delete') {
-                    deleteEvent(info);
-                }
-            },
+            calendar.render();
         });
-
-        calendar.render();
-        
-        function editEvent(info) {
-                var eventTitle = prompt('Edit Event Title:', info.event.title);
-                var newStartDate = prompt('Edit Event Start Date (YYYY-MM-DD HH:mm):', info.event.startStr);
-                var newEndDate = prompt('Edit Event End Date (YYYY-MM-DD HH:mm):', info.event.end ? info.event.endStr : '');
-
-                if (eventTitle !== null && newStartDate !== null) {
-                    info.event.setProp('title', eventTitle);
-                    info.event.setStart(newStartDate);
-                    info.event.setEnd(newEndDate);
-
-                    // Send AJAX request to update the event in the database
-                    $.ajax({
-                        url: '{{ route("calendar-events.update", ":id") }}'.replace(':id', info.event.id),
-                        type: 'PUT',
-                        data: {
-                            title: eventTitle,
-                            start: newStartDate,
-                            end: newEndDate,
-                            _token: '{{ csrf_token() }}'
-                        },
-                        success: function (response) {
-                            calendar.refetchEvents();
-                            console.log(response);
-                        },
-                        error: function (error) {
-                            console.error(error);
-                        }
-                    });
-                } else {
-                    // User clicked Cancel, revert the changes
-                    info.revert();
-                }
-            }
-
-
-        function deleteEvent(info) {
-            if (confirm('Are you sure you want to delete this event?')) {
-                // Send AJAX request to delete the event from the database
-                $.ajax({
-                    url: '{{ route("calendar-events.delete", ":id") }}'.replace(':id', info.event.id),
-                    type: 'DELETE',
-                    data: {
-                        _token: '{{ csrf_token() }}'
-                    },
-                    success: function (response) {
-                        info.event.remove(); // Remove the event from the calendar
-                        console.log(response);
-                    },
-                    error: function (error) {
-                        console.error(error);
-                    }
-                });
-            }
-        }
-    });
     </script>
-
     <!-- End JavaScript for Event Calendar -->
 @endsection
